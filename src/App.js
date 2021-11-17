4d4bb292e90e91d94fdf9bce5f6643464af2cf4f
Zak McRae <zakmcrae@gmail.com>
2021-11-17 14:44:23 -0500

    implented inplace text editing on InputRow

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import InputRow from "./components/InputRow";
 import LabelRow from "./components/LabelRow";
 import TotalsRow from "./components/TotalsRow";
 
+const posterInfo = {
+  id: "001",
+  price: 40,
+  description: "Hamilton, ON Pier 8 Poster June 22, 2019",
+  imgUrl:
+    "https://cdn.shopify.com/s/files/1/0549/2034/7823/products/the-national-hamilton-on-pier-8-poster_4dd452d6-323a-420d-8cef-c0f21c5df80c_5000x.png?v=1635448244",
+};
+
 function App() {
-  const [countStore, setCountScore] = useState({});
+  const [countStore, setCountScore] = useState({
+    qtyAvailable: 0,
+    countIn: 0,
+    add: 0,
+    totalIn: 0,
+    comp: 0,
+    countOut: 0,
+    totalSold: 0,
+    gross: 0,
+  });
 
   return (
     <div className="App">
       <div className="counts-grid">
         <LabelRow></LabelRow>
-        <InputRow></InputRow>
+        <InputRow
+          countStore={countStore}
+          setCountScore={setCountScore}
+          posterInfo={posterInfo}
+        ></InputRow>
         <TotalsRow></TotalsRow>
+        {/* todo remove debugging button  */}
+        <button onClick={() => console.log(countStore)}>log countStore</button>
       </div>
     </div>
   );
--- src/components/InputRow.js
+++ src/components/InputRow.js
@@ -1,14 +1,120 @@
-function InputRow() {
+import { useState } from "react";
+
+function InputRow(props) {
+  const [editMode, setEditMode] = useState(false);
+
+  const {
+    qtyAvailable,
+    countIn,
+    add,
+    totalIn,
+    comp,
+    countOut,
+    totalSold,
+    gross,
+  } = props.countStore;
+
+  const enableEditMode = (e) => {
+    setEditMode(e.target.id);
+  };
+
+  const removeEditMode = (e) => {
+    setEditMode(false);
+  };
+
+  const updateValue = (e) => {
+    console.log(e.target.id, e.target.value);
+    props.setCountScore((prevSetCountScore) => {
+      return { ...prevSetCountScore, [e.target.id]: e.target.value };
+    });
+  };
+
   return (
     <div className="input-row">
-      <div className="input-element black-text">0.00</div>
-      <div className="input-element black-text">0.00</div>
-      <div className="input-element green-text">0.00</div>
-      <div className="input-element blue-text">0.00</div>
-      <div className="input-element red-text">0.00</div>
-      <div className="input-element black-text">0.00</div>
-      <div className="input-element blue-text">0.00</div>
-      <div className="input-element blue-text">0.00</div>
+      <div
+        className="input-element black-text"
+        id="qtyAvailable"
+        onClick={enableEditMode}
+      >
+        {qtyAvailable}
+      </div>
+
+      {editMode === "countIn" ? (
+        <input
+          id="countIn"
+          placeholder={countIn}
+          autoFocus
+          onChange={updateValue}
+          onBlur={removeEditMode}
+        ></input>
+      ) : (
+        <div
+          className="input-element black-text"
+          id="countIn"
+          onClick={enableEditMode}
+        >
+          {countIn}
+        </div>
+      )}
+
+      {editMode === "add" ? (
+        <input
+          id="add"
+          placeholder={add}
+          autoFocus
+          onChange={updateValue}
+          onBlur={removeEditMode}
+        ></input>
+      ) : (
+        <div
+          className="input-element green-text"
+          id="add"
+          onClick={enableEditMode}
+        >
+          {add}
+        </div>
+      )}
+
+      <div className="input-element blue-text">{totalIn}</div>
+
+      {editMode === "comp" ? (
+        <input
+          id="comp"
+          placeholder={comp}
+          autoFocus
+          onChange={updateValue}
+          onBlur={removeEditMode}
+        ></input>
+      ) : (
+        <div
+          className="input-element red-text"
+          id="comp"
+          onClick={enableEditMode}
+        >
+          {comp}
+        </div>
+      )}
+
+      {editMode === "countOut" ? (
+        <input
+          id="countOut"
+          placeholder={countOut}
+          autoFocus
+          onChange={updateValue}
+          onBlur={removeEditMode}
+        ></input>
+      ) : (
+        <div
+          className="input-element black-text"
+          id="countOut"
+          onClick={enableEditMode}
+        >
+          {countOut}
+        </div>
+      )}
+
+      <div className="input-element blue-text">{totalSold}</div>
+      <div className="input-element blue-text">{gross.toFixed(2)}</div>
     </div>
   );
 }
