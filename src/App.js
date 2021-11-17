8b1bd68bc3357cb2a149b613288dbdbba5f6cb0d
Zak McRae <zakmcrae@gmail.com>
2021-11-17 15:16:29 -0500

    changed price to match given example numbers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TotalsRow from "./components/TotalsRow";
 
 const posterInfo = {
   id: "001",
-  price: 40,
+  price: 10,
   description: "Hamilton, ON Pier 8 Poster June 22, 2019",
   imgUrl:
     "https://cdn.shopify.com/s/files/1/0549/2034/7823/products/the-national-hamilton-on-pier-8-poster_4dd452d6-323a-420d-8cef-c0f21c5df80c_5000x.png?v=1635448244",
