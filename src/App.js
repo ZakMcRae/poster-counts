6a6ab8bf530b443143067315cb9a16710d742802
Zak McRae <zakmcrae@gmail.com>
2021-11-18 14:02:14 -0500

    added some comments to explain state usage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import LabelRow from "./components/LabelRow";
 import TotalsRow from "./components/TotalsRow";
 
 function App() {
+  // keep track of whether counts grid can be modified or not
+  const [settledStatus, setSettledStatus] = useState("unsettled");
+
+  // keep track of poster info
   const [posterInfo, setPosterInfo] = useState({
     id: "001",
     price: 10,
@@ -13,6 +17,8 @@ function App() {
     imgUrl:
       "https://cdn.shopify.com/s/files/1/0549/2034/7823/products/the-national-hamilton-on-pier-8-poster_4dd452d6-323a-420d-8cef-c0f21c5df80c_5000x.png?v=1635448244",
   });
+
+  // keep track of counts, quantities,... in counts grid
   const [countStore, setCountScore] = useState({
     qtyAvailable: 0,
     countIn: 0,
