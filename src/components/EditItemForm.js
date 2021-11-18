e0586040d40a1a0c4660f29d475ada2694248b10
Zak McRae <zakmcrae@gmail.com>
2021-11-18 11:20:47 -0500

    added pop edit item menu under more button

---
+++ src/components/EditItemForm.js
@@ -0,0 +1,46 @@
+function EditItemForm(props) {
+  const { price, description } = props.posterInfo;
+  const { setPosterInfo, toggleEditFormStatus } = props;
+
+  const submitEditForm = (e) => {
+    e.preventDefault();
+    setPosterInfo((prevPosterInfo) => {
+      return {
+        ...prevPosterInfo,
+        price: e.target[0].value,
+        description: e.target[1].value,
+      };
+    });
+    toggleEditFormStatus();
+  };
+
+  return (
+    <div className="pop-up">
+      <div className="arrow-up"></div>
+      <div className="edit-form">
+        <p>Edit Item</p>
+        <form action="#" onSubmit={submitEditForm}>
+          <label htmlFor="price">Price</label>
+          <input
+            autoFocus
+            className="price-input"
+            type="text"
+            name="price"
+            defaultValue={price}
+          />
+          <label htmlFor="description">Description</label>
+          <textarea
+            className="description-input"
+            type="text"
+            name="description"
+            defaultValue={description}
+            rows="4"
+          />
+          <button>OK</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default EditItemForm;
--- src/components/TotalsRow.js
+++ src/components/TotalsRow.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
+import EditItemForm from "./EditItemForm";
+
 function TotalsRow(props) {
   const { totalIn, comp, countOut, totalSold, gross } = props.countStore;
+  const { posterInfo, setPosterInfo } = props;
+  const [editFormStatus, setEditFormStatus] = useState(false);
+
+  const toggleEditFormStatus = () => {
+    setEditFormStatus((prevEditFormStatus) => !prevEditFormStatus);
+  };
 
   return (
-    <div className="totals-row">
-      <div className="button-container">
-        <button className="more-rows-button">More</button>
+    <>
+      <div className="totals-row">
+        <div className="button-container">
+          <button className="more-rows-button" onClick={toggleEditFormStatus}>
+            More
+          </button>
+        </div>
+        <div className="totals-element blue-text">{totalIn}</div>
+        <div className="totals-element red-text">{comp}</div>
+        <div className="totals-element black-text">{countOut}</div>
+        <div className="totals-element blue-text">{totalSold}</div>
+        <div className="totals-element blue-text">${gross.toFixed(2)}</div>
       </div>
-      <div className="totals-element blue-text">{totalIn}</div>
-      <div className="totals-element red-text">{comp}</div>
-      <div className="totals-element black-text">{countOut}</div>
-      <div className="totals-element blue-text">{totalSold}</div>
-      <div className="totals-element blue-text">${gross.toFixed(2)}</div>
-    </div>
+      <div className="form-row">
+        {editFormStatus ? (
+          <EditItemForm
+            posterInfo={posterInfo}
+            setPosterInfo={setPosterInfo}
+            toggleEditFormStatus={toggleEditFormStatus}
+          />
+        ) : null}
+      </div>
+    </>
   );
 }
 
