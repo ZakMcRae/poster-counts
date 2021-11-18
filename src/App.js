2da86d7a094331da78259318185fc270990e27d9
Zak McRae <zakmcrae@gmail.com>
2021-11-18 11:19:15 -0500

    moved posterInfo into state
    needs to be passed to edit item form

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import InputRow from "./components/InputRow";
 import LabelRow from "./components/LabelRow";
 import TotalsRow from "./components/TotalsRow";
 
-const posterInfo = {
-  id: "001",
-  price: 10,
-  description: "Hamilton, ON Pier 8 Poster June 22, 2019",
-  imgUrl:
-    "https://cdn.shopify.com/s/files/1/0549/2034/7823/products/the-national-hamilton-on-pier-8-poster_4dd452d6-323a-420d-8cef-c0f21c5df80c_5000x.png?v=1635448244",
-};
-
 function App() {
+  const [posterInfo, setPosterInfo] = useState({
+    id: "001",
+    price: 10,
+    description: "Hamilton, ON Pier 8 Poster June 22, 2019",
+    imgUrl:
+      "https://cdn.shopify.com/s/files/1/0549/2034/7823/products/the-national-hamilton-on-pier-8-poster_4dd452d6-323a-420d-8cef-c0f21c5df80c_5000x.png?v=1635448244",
+  });
   const [countStore, setCountScore] = useState({
     qtyAvailable: 0,
     countIn: 0,
@@ -35,9 +34,11 @@ function App() {
           setCountScore={setCountScore}
           posterInfo={posterInfo}
         ></InputRow>
-        <TotalsRow countStore={countStore}></TotalsRow>
-        {/* todo remove debugging button  */}
-        <button onClick={() => console.log(countStore)}>log countStore</button>
+        <TotalsRow
+          countStore={countStore}
+          posterInfo={posterInfo}
+          setPosterInfo={setPosterInfo}
+        ></TotalsRow>
       </div>
     </div>
   );
