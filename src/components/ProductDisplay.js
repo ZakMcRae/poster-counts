5f4ac194556c16919f25671abb80f7a299e158ad
Zak McRae <zakmcrae@gmail.com>
2021-11-22 14:17:44 -0500

    added poster note functionality
    if a note exists, displays an exclamation mark
    on hover of that, the not will be displayed

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -1,13 +1,38 @@
-import { FaPlus, FaBoxOpen, FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
+import {
+  FaPlus,
+  FaBoxOpen,
+  FaShoppingCart,
+  FaExclamationCircle,
+} from "react-icons/fa";
 
 function ProductDisplay(props) {
-  const { price, imgUrl } = props.posterInfo;
+  const [noteHover, setNoteHover] = useState(false);
+  const { price, imgUrl, description } = props.posterInfo;
 
   return (
     <div className="product-display">
       <div className="left-product-display">
-        <p>Poster</p>
+        {/* if a description/note exist show exclamation indicator for note on hover */}
+        {description === "" ? null : (
+          <FaExclamationCircle
+            className="poster-note-notification"
+            onMouseEnter={() => {
+              setNoteHover(true);
+            }}
+            onMouseLeave={() => {
+              setNoteHover(false);
+            }}
+          />
+        )}
         <img className="product-image" src={imgUrl} alt="Concert Poster" />
+        {/* display poster note if it exists and user hovers on exclamation indicator */}
+        {noteHover ? (
+          <div className="poster-note">
+            <div className="poster-note-arrow-up"></div>
+            <p>{description}</p>
+          </div>
+        ) : null}
         <div className="icons">
           <FaPlus />
           <FaBoxOpen />
