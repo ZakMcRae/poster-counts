710ff10b02ecde793faa244363f5f01c85beb819
Zak McRae <zakmcrae@gmail.com>
2021-11-23 13:48:30 -0500

    fixed warnings for css properties
    fixed to react friendly names

--- src/components/ProgressCircle.js
+++ src/components/ProgressCircle.js
@@ -8,22 +8,22 @@ function ProgressCircle(props) {
         <circle
           className="background-ring"
           stroke="#979797"
-          stroke-width="4"
+          strokeWidth="4"
           fill="transparent"
           r="22"
           cx="25"
           cy="25"
-          style={{ "z-index": "-1" }}
+          style={{ zIndex: "-1" }}
         />
         <circle
           className="percent-ring"
           stroke="#00c7fc"
-          stroke-width="4"
+          strokeWidth="4"
           fill="transparent"
           r="22"
           cx="25"
           cy="25"
-          style={{ strokeDashoffset: offset }}
+          style={{ strokeDashoffset: offset || 0 }}
         />
       </svg>
       <p>{totalSold}</p>
