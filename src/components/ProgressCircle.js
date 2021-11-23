087a231bcb5ed83fa63b40c075496d30a8b851d8
Zak McRae <zakmcrae@gmail.com>
2021-11-23 10:03:33 -0500

    background ring now identical to percent ring
    circle previously made with border-radius 50% now same svg

--- src/components/ProgressCircle.js
+++ src/components/ProgressCircle.js
@@ -6,6 +6,16 @@ function ProgressCircle(props) {
     <div className="percent-circle">
       <svg width="50" height="50">
         <circle
+          className="background-ring"
+          stroke="#979797"
+          stroke-width="4"
+          fill="transparent"
+          r="22"
+          cx="25"
+          cy="25"
+          style={{ "z-index": "-1" }}
+        />
+        <circle
           className="percent-ring"
           stroke="#00c7fc"
           stroke-width="4"
